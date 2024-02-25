e6d143e6d7dc978a1746bbd3b7a132d81e254ddf
LucaB98 <lucabellan98@gmail.com>
2024-02-25 18:34:31 +0100

    add construct and function

--- index.php
+++ index.php
@@ -1,1 +1,23 @@
 <?php
+class Movie
+{
+    public $title;
+    public $director;
+    public $actor;
+
+    public function __construct($title, $director, $actor)
+    {
+        $this->title = $title;
+        $this->director = $director;
+        $this->actor = $actor;
+    }
+
+    public function infoMovie()
+    {
+        echo $this->title . ' ' . $this->director;
+    }
+}
+
+$movie1 = new Movie('amazon', 'marco', 'george');
+
+$movie1->infoMovie();

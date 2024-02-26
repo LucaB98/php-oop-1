6c1dc201a5a07125b668ebf0950b22df7f4232fe
LucaB98 <lucabellan98@gmail.com>
2024-02-26 16:03:18 +0100

    add cast

--- index.php
+++ index.php
@@ -1,5 +1,19 @@
 <?php
+include_once __DIR__ . '/models/movie.php';
+include_once __DIR__ . '/models/actor.php';
+include_once __DIR__ . '/models/director.php';
 
+$director = new Director(1, 'Peter', 'Jackson');
+
+$actor1 = new Actor(1, 'Ryan', 'Renols');
+$actor2 = new Actor(2, 'Robert', 'daweyn');
+
+$actors = [$actor1, $actor2];
+
+$movie1 = new Movie(1, 'Deadpool', 'Violence', $director, $actors);
+$movie2 = new Movie(2, 'IronMan', 'sci-fi', $director, $actors);
+
+$movies = [$movie1, $movie2];
 ?>
 
 <!DOCTYPE html>
@@ -10,6 +24,15 @@
     <title>Movie</title>
 </head>
 <body>
-    
+    <ul>
+        <?php foreach($movies as $movie):?>
+            <li>
+                <h2><?= $movie->title ?></h2>
+                <p><?= $movie->genre ?></p>
+                <address><?= $movie->director->getFullName() ?></address>
+                <p>CAST: <?= $movie->getCastNames() ?></p>
+            </li>
+        <?php endforeach?>
+    </ul>
 </body>
 </html>
--- models/movie.php
+++ models/movie.php
@@ -1,22 +1,32 @@
 <?php
+
+include_once __DIR__ . '/director.php';
+
 class Movie
 {
     public $title;
     public $director;
     public $id;
     public $genre;
+    public $cast;
 
-    public function __construct($id, $title, $genre, $director)
+    public function __construct($id, $title, $genre, Director $director, array $cast = [])
     {
         $this->id = $id;
         $this->title = $title;
         $this->director = $director;
         $this->genre = $genre;
+        $this->cast = $cast;
     }
 
+    public function getCastNames()
+    {
+        $names = '';
+        foreach($this->cast as $actor){
+            $names .= $actor->getFullName() . ', ';
+        }
 
+        return substr($names, 0, -2);
+    }
 }
 
-$movie1 = new Movie('amazon', 'marco', 'george');
-
-$movie1->infoMovie();
